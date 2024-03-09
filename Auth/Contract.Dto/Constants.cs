0ace3351efdbf7d80178c8822ec73178e95fcb63
Aleksey Sosnovsky <a.sosnovskiy@2gis.ru>
2024-03-09 16:37:43 +0700

    [*] - fix topic names

--- Auth/Contract.Dto/Constants.cs
+++ Auth/Contract.Dto/Constants.cs
@@ -3,7 +3,6 @@ namespace Contract.Dto;
 public static class Constants
 {
     public static string UsersStreamTopicName => "users-stream";
-    public static string UsersTopicName => "users";
     public static string TasksStreamTopicName => "tasks-stream";
-    public static string TasksTopicName => "tasks";
+    public static string TasksWorkflowTopicName => "tasks-workflow";
 }
--- Tasks/Contract.Dto/Events/Constants.cs
+++ Tasks/Contract.Dto/Events/Constants.cs
@@ -3,7 +3,6 @@ namespace Contract.Dto.Events;
 public static class Constants
 {
     public static string UsersStreamTopicName => "users-stream";
-    public static string UsersTopicName => "users";
     public static string TasksStreamTopicName => "tasks-stream";
-    public static string TasksTopicName => "tasks";
+    public static string TasksWorkflowTopicName => "tasks-workflow";
 }
